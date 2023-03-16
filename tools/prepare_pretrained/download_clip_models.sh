7b790cb8ef7b3dd542871133307e3b310eaa4ca1
ChiaYi-LIN <b04704016@ntu.edu.tw>
2023-03-16 21:50:32 +0800

    download CLIP checkpoints

---
+++ tools/prepare_pretrained/download_clip_models.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+root="./pretrained/"
+mkdir -p $root
+
+wget https://openaipublic.azureedge.net/clip/models/afeb0e10f9e5a86da6080e35cf09123aca3b358a0c3e3b6c78a7b63bc04b6762/RN50.pt -P $root
