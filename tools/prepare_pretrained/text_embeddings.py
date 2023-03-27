792c24ad530d8bc8c0a1dc1cc76a6437858dd000
ChiaYi-LIN <b04704016@ntu.edu.tw>
2023-03-27 17:37:45 +0800

    generate CLIP text features for each dataset

--- tools/prepare_pretrained/text_embeddings.py
+++ tools/prepare_pretrained/text_embeddings.py
@@ -1,10 +1,10 @@
 import torch
 import clip
+from sklearn.decomposition import PCA
 from mmseg.datasets import build_dataset
 
 
-def get_dataset_classes(dataset_type='ADE20KDataset'):
-    data_root = 'data/ADEChallengeData2016'
+def get_dataset_classes(dataset_cfg):
     IMG_MEAN = [v*255 for v in [0.48145466, 0.4578275, 0.40821073]]
     IMG_VAR = [v*255 for v in [0.26862954, 0.26130258, 0.27577711]]
 
@@ -24,10 +24,10 @@ def get_dataset_classes(dataset_type='ADE20KDataset'):
     ]
 
     cfg = dict(
-        type=dataset_type,
-        data_root=data_root,
-        img_dir='images/training',
-        ann_dir='annotations/training',
+        type=dataset_cfg['dataset_type'],
+        data_root=dataset_cfg['data_root'],
+        img_dir=dataset_cfg['img_dir'],
+        ann_dir=dataset_cfg['img_dir'],
         pipeline=train_pipeline
     )
 
@@ -35,17 +35,47 @@ def get_dataset_classes(dataset_type='ADE20KDataset'):
     return list(datasets[0].CLASSES)
 
 
-def get_text_embeddings(dataset_type, which_clip):
-    class_names = get_dataset_classes(dataset_type)
-    model, _ = clip.load(which_clip, device='cpu')
+def get_text_embeddings(dataset_cfg, model_name):
+    class_names = get_dataset_classes(dataset_cfg)
+    model, _ = clip.load(model_name, device='cpu')
     texts = torch.cat([clip.tokenize(c).to('cpu') for c in class_names])
     return model.encode_text(texts)
 
 
+def perform_pca(text_embeddings, num_class):
+    x = text_embeddings.detach().numpy()
+    pca = PCA(n_components=num_class)
+    x_pca = pca.fit_transform(x)
+    return torch.from_numpy(x_pca)
+
+
 if __name__ == "__main__":
-    dataset_type = 'ADE20KDataset'
-    dataset_short = 'ade20k'
-    which_clip = "RN50"
-    text_embeddings = get_text_embeddings(dataset_type=dataset_type, which_clip=which_clip)
+    cfg = dict(
+        city=dict(
+            dataset_type='CityscapesDataset',
+            data_root='data/cityscapes/',
+            img_dir='leftImg8bit/train',
+            ann_dir='gtFine/train',
+            num_class=19,
+        ),
+        ade20k=dict(
+            dataset_type='ADE20KDataset',
+            data_root='data/ade/ADEChallengeData2016',
+            img_dir='images/training',
+            ann_dir='annotations/training',
+            num_class=150,
+        )
+    )
+
+    dataset = 'city'
+    model_name = 'RN101'
+    dataset_cfg = cfg[dataset]
+
+    text_embeddings = get_text_embeddings(dataset_cfg, model_name)
+    model_name = model_name.replace("/", "")
     K, C = text_embeddings.shape
-    torch.save(text_embeddings, f"./pretrained/textfeat_{dataset_short}_{K}_{which_clip}_{C}.pth")
+    torch.save(text_embeddings, f"./pretrained/textfeat_{dataset}_{K}_{model_name}_{C}.pth")
+
+    text_embeddings_pca = perform_pca(text_embeddings, dataset_cfg['num_class'])
+    K, C = text_embeddings_pca.shape
+    torch.save(text_embeddings_pca, f"./pretrained/textfeat_{dataset}_{K}_{model_name}_{C}.pth")
