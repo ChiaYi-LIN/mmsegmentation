47e3aedb98367d644b43291e0019b29180a57ec2
ChiaYi-LIN <b04704016@ntu.edu.tw>
2023-04-12 19:22:58 +0800

    correct eval shape, add scale step

--- configs/_base_/datasets/camvid.py
+++ configs/_base_/datasets/camvid.py
@@ -7,7 +7,7 @@ crop_size = (720, 960)
 train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations'),
-    dict(type='Resize', img_scale=(960, 720), ratio_range=(0.5, 2.5)),
+    dict(type='Resize', img_scale=(960, 720), ratio_range=(0.5, 2.5), scale_step_size=0.25),
     dict(type='RandomCrop', crop_size=crop_size, cat_max_ratio=0.75),
     dict(type='RandomFlip', prob=0.5),
     dict(type='PhotoMetricDistortion'),
@@ -20,7 +20,7 @@ test_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(
         type='MultiScaleFlipAug',
-        img_scale=(2048, 720),
+        img_scale=(960, 720),
         # img_ratios=[0.5, 0.75, 1.0, 1.25, 1.5, 1.75],
         flip=False,
         transforms=[
